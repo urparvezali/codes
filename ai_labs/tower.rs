2f6ccf78c394ce4519a6799454df41497ef0074e
Parvez Ali <urparvezali@gmail.com>
2024-10-25 16:38:30 +0600

    microcontroller

--- ai_labs/tower.rs
+++ ai_labs/tower.rs
@@ -1,26 +1,15 @@
 use std::io::{stdin, stdout, BufRead, Write};
 fn main() {
-    print!("Enter number of disks: ");
-    stdout().flush().unwrap();
-    let n = stdin()
-        .lock()
-        .lines()
-        .next()
-        .unwrap()
-        .unwrap()
-        .parse::<isize>()
-        .unwrap();
-    let mut count = 0;
-    recursion(n, &'A', &'B', &'C', &mut count);
-    println!("The number of total moves needed: {}", count);
-    stdout().flush().unwrap();
+	print!("Enter number of disks: ");
+	stdout().flush().unwrap();
+	let n = stdin().lock().lines().next().unwrap().unwrap().parse::<isize>().unwrap();
+	tofh(n, &'A', &'B', &'C');
 }
-fn recursion(n: isize, source: &char, helper: &char, dest: &char, count: &mut usize) {
-    if n == 0 {
-        return;
-    }
-    recursion(n - 1, source, dest, helper, count);
-    // println!("Disk {} moved from {} to {}", n, source, dest);
-    *count += 1;
-    recursion(n - 1, helper, source, dest, count);
+fn tofh(n: isize, source: &char, helper: &char, dest: &char) {
+	if n == 0 {
+		return;
+	}
+	tofh(n - 1, source, dest, helper);
+	println!("Disk {} moved from {} to {}", n, source, dest);
+	tofh(n - 1, helper, source, dest);
 }
